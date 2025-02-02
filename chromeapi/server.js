8d1b507325df824e314e16c7916eb5d6dd950355
Sumit Sagar <sumitsagar2612@gmail.com>
2025-02-02 06:19:53 +0530

    fixed DB

--- chromeapi/server.js
+++ chromeapi/server.js
@@ -4,17 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE
-    .replace("<USERNAME>", encodeURIComponent(process.env.DATABASE_USERNAME))
-    .replace("<PASSWORD>", encodeURIComponent(process.env.DATABASE_PASSWORD));
+const DB = process.env.DATABASE;
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(DB)
   .then(() => console.log('DB connection successful!'))
   .catch((err) => {
     console.error('DB connection error:', err);
